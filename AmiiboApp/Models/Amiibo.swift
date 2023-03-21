3c478278d6c084c6b239bb708f05cee395b915dd
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-21 17:16:51 -0300

    Model setup

--- AmiiboApp/Models/Amiibo.swift
+++ AmiiboApp/Models/Amiibo.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct AmiiboList: Decodable {
+    let amiibo: [Amiibo]
+}
+
+struct Amiibo: Decodable {
+    let amiiboSeries: String?
+    let character: String?
+    let gameSeries: String?
+    let image: URL?
+    let name: String?
+    let release: Release?
+    let type: String?
+}
+
+struct Release: Decodable {
+    let eu: String?
+}
