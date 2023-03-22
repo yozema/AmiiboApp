5d98854d5d860875e008946357948790c65d7f63
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-21 22:14:55 -0300

    code organization

--- AmiiboApp/ViewControllers/AmiiboViewController.swift
+++ AmiiboApp/ViewControllers/AmiiboViewController.swift
@@ -8,32 +8,10 @@
 import UIKit
 
 final class AmiiboViewController: UIViewController {
-    
-    private let amiibosURL = "https://amiiboapi.com/api/amiibo/?showusage"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchAmiibos()
-    }
-    
-    private func fetchAmiibos() {
-        guard let url = URL(string: amiibosURL) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                let amiiboList = try decoder.decode(Amiibo.self, from: data)
-                print(amiiboList.amiibo)
-            } catch let error {
-                print(error)
-            }
-        }.resume()
+        FetchAmiibo.shared.fetchAmiibo()
     }
 }
 
