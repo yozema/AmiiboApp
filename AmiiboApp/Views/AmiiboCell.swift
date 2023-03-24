5326a857e7da8fdd245708ac253ab6b9310f5b3d
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-24 20:39:06 -0300

    AmiiboCell setup

--- AmiiboApp/Views/AmiiboCell.swift
+++ AmiiboApp/Views/AmiiboCell.swift
@@ -5,4 +5,28 @@
 //  Created by Ilya Zemskov on 24.03.2023.
 //
 
-import Foundation
+import UIKit
+
+final class AmiiboCell: UICollectionViewCell {
+    @IBOutlet var amiiboImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var gameLabel: UILabel!
+    @IBOutlet var seriesLabel: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    
+    func configure(with amiibo: Description) {
+        nameLabel.text = amiibo.character
+        gameLabel.text = amiibo.gameSeries
+        seriesLabel.text = amiibo.amiiboSeries
+        
+        networkManager.fetchImage(from: amiibo.image) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.amiiboImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
