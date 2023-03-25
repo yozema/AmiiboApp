5a56fa6cee11a9297f70d9ba5d33edc84c3680ea
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-25 00:02:05 -0300

    AmiiboCell logic setup

--- AmiiboApp/Views/AmiiboCell.swift
+++ AmiiboApp/Views/AmiiboCell.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 final class AmiiboCell: UICollectionViewCell {
+    // MARK: - IBOutlets
     @IBOutlet var amiiboImage: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var gameLabel: UILabel!
     @IBOutlet var seriesLabel: UILabel!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
+    // MARK: - Properties
     private let networkManager = NetworkManager.shared
     
+    // MARK: - Cell Configuration
     func configure(with amiibo: Description) {
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
         nameLabel.text = amiibo.character
         gameLabel.text = amiibo.gameSeries
         seriesLabel.text = amiibo.amiiboSeries
@@ -24,6 +30,7 @@ final class AmiiboCell: UICollectionViewCell {
             switch result {
             case .success(let imageData):
                 self?.amiiboImage.image = UIImage(data: imageData)
+                self?.activityIndicator.stopAnimating()
             case .failure(let error):
                 print(error)
             }
