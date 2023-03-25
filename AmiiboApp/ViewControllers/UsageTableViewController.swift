6ca3413222c5971c61b276bbc48d88ddef2b8f14
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-25 18:40:56 -0300

    Games usage set up

--- AmiiboApp/ViewControllers/UsageTableViewController.swift
+++ AmiiboApp/ViewControllers/UsageTableViewController.swift
@@ -7,83 +7,97 @@
 
 import UIKit
 
-class UsageTableViewController: UITableViewController {
+enum Consoles: String {
+    case nintendoSwitch = "Nintendo Switch"
+    case nintendoWiiU = "Nintendo WiiU"
+    case nintendo3DS = "Nintendo 3DS"
+}
 
+final class UsageTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var amiiboDescription: Description!
+    
+    private let networkManager = NetworkManager.shared
+    private var consolesQuantity = 0
+    private var gameList: [String] = []
+    private var switchGames: [String] = []
+    private var wiiuGames: [String] = []
+    private var threeDSGames: [String] = []
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = amiiboDescription.name
+        
+        prepareGameList()
+        consolesCounting()
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        consolesQuantity
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sectionName: String
+        switch section {
+        case 0:
+            sectionName = Consoles.nintendoSwitch.rawValue
+        case 1:
+            sectionName = Consoles.nintendoWiiU.rawValue
+        default:
+            sectionName = Consoles.nintendo3DS.rawValue
+        }
+        return sectionName
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let sectionRows: Int
+        switch section {
+        case 0:
+            sectionRows = switchGames.count
+            print(sectionRows)
+            print(switchGames)
+        case 1:
+            sectionRows = wiiuGames.count
+        default:
+            sectionRows = threeDSGames.count
+        }
+        return sectionRows
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gameUse", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        cell.contentConfiguration = content
+        switch indexPath.section {
+        case 0:
+            content.text = switchGames[indexPath.row]
+        case 1:
+            content.text = wiiuGames[indexPath.row]
+        default:
+            content.text = threeDSGames[indexPath.row]
+        }
+        cell.contentConfiguration = content
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    // MARK: - Private functions
+    private func prepareGameList() {
+        switchGames = amiiboDescription.gamesSwitch.map { $0.gameName }
+        wiiuGames = amiiboDescription.gamesWiiU.map { $0.gameName }
+        threeDSGames = amiiboDescription.games3DS.map { $0.gameName }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    private func consolesCounting() {
+        for item in amiiboDescription.consoles {
+            consolesQuantity += item
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
